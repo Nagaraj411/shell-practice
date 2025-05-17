a10f0023bf06526e83015e8b20fc2566f02fddb6
Nagaraj <nagarajuande255@gmail.com>
2025-05-17 08:14:42 +0530

    hello-world

--- hello-world.sh
+++ hello-world.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-123
-363
-56566
-56566
-6565
-666
+echo "123363"
 echo "Hello, World!"
-nayanatara
-anusha 
-eliyna
-gopika
-julia
-sree
+echo "nayanatara"
+echo "anusha"
+echo "eliyna"
+echo "gopika"
+echo "julia"
+echo "sree"
