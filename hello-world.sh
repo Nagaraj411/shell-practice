eed30b4ab9eac33634bc97d5d37032c644eee01d
Nagaraj <nagarajuande255@gmail.com>
2025-05-17 09:33:00 +0530

    hello-world

--- hello-world.sh
+++ hello-world.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-echo "123363"
 echo "Hello, World!"
-echo "nayanatara"
-echo "anusha"
-echo "eliyna"
-echo "gopika"
-echo "julia"
-echo "sree"
-echo "sreeja"
-echo "sree"
-echo "sree"
-echo "sree"
