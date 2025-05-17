7f44fe1d99f71d96c531dadbd58a9f7cf74dc889
Nagaraj <nagarajuande255@gmail.com>
2025-05-17 10:48:04 +0530

    variables.sh

--- variables.sh
+++ variables.sh
@@ -1,6 +1,7 @@
 #!bit/bash
 
-PERSON1=Nagaraju
+PERSON1=Nagaraju    #there should be not space around before & abfter for =
+
 
 echo "$PERSON1 : mera bharat mahan hai"
 echo "$PERSON1 : mera bharat mata jai"
