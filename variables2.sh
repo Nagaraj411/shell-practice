6bfd5f3b08ced52856683c892dcafa0ccd5b3ce3
Nagaraj <nagarajuande255@gmail.com>
2025-05-17 10:50:29 +0530

    variables2.sh

---
+++ variables2.sh
@@ -0,0 +1,8 @@
+##!/bin/bash
+
+PERSON1=$1    #there should be not space around before & abfter for =
+
+
+echo "$PERSON1 : mera bharat mahan hai"
+echo "$PERSON1 : mera bharat mata jai"
+echo "$PERSON1 : mera bharat mata ki jai INDIA"
