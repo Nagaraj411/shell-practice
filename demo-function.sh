58f5661d029185648a7c1f8c6b794b883998f289
Nagaraj <nagarajuande255@gmail.com>
2025-05-18 21:10:32 +0530

    demo-function

--- demo-function.sh
+++ demo-function.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
- if [ $USERID -eq 0 ]    then
+if [ $USERID -eq 0 ]    
+then
  echo "Please run with sudo user"
-    else {
+else 
     echo "Running with sudo user"
     exit 1
-    if
-    }
+fi
