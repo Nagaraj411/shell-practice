e155d08efda634deaa09fb0769e251225af7322d
Nagaraj <nagarajuande255@gmail.com>
2025-05-19 11:44:28 +0530

    demo-function.sh

--- demo-function.sh
+++ demo-function.sh
@@ -9,9 +9,10 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "❌ Run with sudo user to install packages" #first lone print message if 0 equal to 0
-    exit 1
+
 else
     echo "✅ Running with sudo user..."
+    exit 1
 fi          #9-15 line are root user checking commands
 
 #function to validate package installed succesfully or not
