6845a2e338a5515448c8a39cd591ef2d0ab9dc31
Nagaraj <nagarajuande255@gmail.com>
2025-05-25 10:15:58 +0530

    shell-practice

--- delecting-oldfile.sh
+++ delecting-oldfile.sh
@@ -3,6 +3,6 @@
 while IFS= read -r line          #Internal field separator
 do
 echo "Processing line: $line"
-  if (find . -name *.log * -mtime +14); then
+  if ( find . -name *.log * -mtime +60 ); then
     echo "Deleting old file: $line"
 done
