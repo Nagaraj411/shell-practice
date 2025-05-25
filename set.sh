ee3922376fef0b0de42662ff90377558eb458847
Nagaraj <nagarajuande255@gmail.com>
2025-05-25 09:21:24 +0530

    shell-practice

--- set.sh
+++ set.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
 echo "Hello, World!"
 echoooooooooo   "name is Nagaraju"
 echo "This is a shell script practice file."
